35eecff44455de287f80e94850077a63d3d9761b
Cindy Taing <cindyhtaing@gmail.com>
2021-03-03 16:09:03 -0800

    Add popup marker and styles

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const form = document.querySelector(".form");
+const inputRestaurant = document.querySelector(".form__input--restaurant");
+
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -9,20 +14,39 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
       console.log(coords);
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapE) {
+        form.classList.remove("hidden");
+        mapEvent = mapE;
+      });
     },
     function () {
       alert("Unable to get your location");
     }
   );
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          minWidth: 100,
+          maxWidth: 200,
+          autoClose: false,
+          closeOnClick: false,
+        })
+      )
+      .setPopupContent("Food spot")
+      .openPopup();
+  });
 }
