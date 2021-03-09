467fac8c49263a7fc3bf1d145a0af226bdee9876
Cindy Taing <cindyhtaing@gmail.com>
2021-03-08 23:00:49 -0800

    Fix form submit and refactor

--- script.js
+++ script.js
@@ -2,42 +2,81 @@
 
 const form = document.querySelector(".form");
 const inputRestaurant = document.querySelector(".form__input--restaurant");
+const inputRating = document.querySelector(".form__input--rating");
+const inputPrice = document.getElementById("form__input--price");
+const inputDescription = document.querySelector(".form__input--description");
+
+class Log {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, name, rating, price, description) {
+    this.coords = coords;
+    this.name = name;
+    this.rating = rating;
+    this.price = price;
+    this.description = description;
+  }
+}
 
 let map, mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
+class App {
+  #map;
+  #mapEvent;
 
-      const coords = [latitude, longitude];
-      console.log(coords);
+  constructor() {
+    this._getPosition();
 
-      map = L.map("map").setView(coords, 13);
+    form.addEventListener("submit", this._newLog.bind(this));
+  }
 
-      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Unable to get your location");
+        }
+      );
+  }
 
-      map.on("click", function (mapE) {
-        form.classList.remove("hidden");
-        mapEvent = mapE;
-      });
-    },
-    function () {
-      alert("Unable to get your location");
-    }
-  );
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
-  form.addEventListener("submit", function (e) {
+    const coords = [latitude, longitude];
+    console.log(coords);
+
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+  }
+
+  _newLog(e) {
     e.preventDefault();
 
-    const { lat, lng } = mapEvent.latlng;
+    // Get data from form
+    const name = inputRestaurant.value;
+    const rating = inputRating.value;
+    const price = inputPrice.options[inputPrice.selectedIndex].text;
+    const description = inputDescription.value;
+    let log;
+
+    const { lat, lng } = this.#mapEvent.latlng;
 
     L.marker([lat, lng])
-      .addTo(map)
+      .addTo(this.#map)
       .bindPopup(
         L.popup({
           minWidth: 100,
@@ -48,5 +87,14 @@ if (navigator.geolocation) {
       )
       .setPopupContent("Food spot")
       .openPopup();
-  });
+
+    // Create new log object
+    log = new Log([lat, lng], name, rating, price, description);
+    console.log(log);
+
+    // Empty values
+    inputRestaurant.value = inputRating.value = inputDescription.value = "";
+  }
 }
+
+const app = new App();
