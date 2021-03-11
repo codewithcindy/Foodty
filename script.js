cbfb9a8a5a33cf7432af7c0176b00c3af22e4648
Cindy Taing <cindyhtaing@gmail.com>
2021-03-10 17:47:12 -0800

    Add objects to local storage

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const inputRestaurant = document.querySelector(".form__input--restaurant");
 const inputRating = document.querySelector(".form__input--rating");
 const inputPrice = document.getElementById("form__input--price");
 const inputDescription = document.querySelector(".form__input--description");
+const logsContainer = document.querySelector(".logs");
+const sorting = document.querySelector(".sorting");
+const sortingInput = document.querySelector(".sorting__input");
 
 class Log {
   date = new Date();
@@ -24,12 +27,22 @@ let map, mapEvent;
 class App {
   #map;
   #mapEvent;
+  #mapZoomLevel = 13;
   #logs = [];
 
   constructor() {
+    // Get user's current position
     this._getPosition();
 
+    // Retrieve data from local storage
+    this._getLocalStorage();
+
+    // Event handlers
     form.addEventListener("submit", this._newLog.bind(this));
+    logsContainer.addEventListener("click", this._moveToPopup.bind(this));
+
+    if (sortingInput.options[sortingInput.selectedIndex].value === "rating")
+      console.log("rating");
   }
 
   _getPosition() {
@@ -46,10 +59,9 @@ class App {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
-    const coords = [latitude, longitude];
-    console.log(coords);
+    const coords = [34.0201613, -118.4227626];
 
-    this.#map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
@@ -57,22 +69,30 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on("click", this._showForm.bind(this));
+
+    this.#logs.forEach((log) => this._renderLogMarker(log));
   }
 
   _showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove("hidden");
+    sorting.classList.add("hidden");
+  }
+
+  _hideForm() {
+    form.classList.add("hidden");
+    sorting.classList.remove("hidden");
+    inputRestaurant.value = inputRating.value = inputDescription.value = "";
   }
 
   _newLog(e) {
     const validInputs = (...inputs) => inputs.every((inp) => inp.length > 0);
-    const positiveInputs = (...inputs) => inputs.every((inp) => inp > 0);
 
     e.preventDefault();
 
     // Get data from form
     const name = inputRestaurant.value;
-    const rating = inputRating.value;
+    const rating = inputRating.options[inputRating.selectedIndex].text;
     const price = inputPrice.options[inputPrice.selectedIndex].text;
     const description = inputDescription.value;
     const { lat, lng } = this.#mapEvent.latlng;
@@ -83,13 +103,11 @@ class App {
     // Check if data is valid
     if (!validInputs(name, description))
       return alert("Must fill out all fields");
-    if (!positiveInputs(rating)) return alert("Must enter a positive rating");
 
     log = new Log([lat, lng], name, rating, price, description);
 
     // Add object to log array
     this.#logs.push(log);
-    console.log(log);
 
     // Render new log on map as marker
     this._renderLogMarker(log);
@@ -98,10 +116,10 @@ class App {
     this._renderLog(log);
 
     // Hide form + clear input values
-    form.classList.add("hidden");
-    inputRestaurant.value = inputRating.value = inputDescription.value = "";
+    this._hideForm();
 
     // Set local storage
+    this._setLocalStorage();
   }
 
   _renderLogMarker(log) {
@@ -115,25 +133,69 @@ class App {
           closeOnClick: false,
         })
       )
-      .setPopupContent(`${log.name} <br> ${log.rating}/5 ${log.price}`)
+      .setPopupContent(`${log.name} ${log.price}<br> ${log.rating} `)
       .openPopup();
   }
 
   _renderLog(log) {
-    const html = `<li class="log">
+    const html = `<li class="log" data-id=${log.id}>
     <h2>${log.name}</h2>
     <div class="log__details">
       <div class="log__details--left">
         ${log.description}
       </div>
       <div class="log__details--right">
-        <div class="details__rating">${log.rating}/5</div>
+        <div class="details__rating">${log.rating}</div>
         <div class="details__price">${log.price}</div>
       </div>
     </div>
   </li>`;
 
     form.insertAdjacentHTML("afterend", html);
+  }
+
+  _sortLogsByRating() {
+    this.#logs.sort(function (a, b) {
+      if (a.rating < b.rating) return 1;
+    });
+  }
+
+  _moveToPopup(e) {
+    const logEl = e.target.closest(".log");
+
+    if (!logEl) return;
+
+    const log = this.#logs.find((log) => log.id === logEl.dataset.id);
+
+    this.#map.setView(log.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem("logs", JSON.stringify(this.#logs));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("logs"));
+
+    if (!data) {
+      sorting.classList.add("hidden");
+      return;
+    }
+
+    this.#logs = data;
+
+    this.#logs.forEach((log) => this._renderLog(log));
+    console.log(this.#logs);
+  }
+
+  reset() {
+    localStorage.removeItem("logs");
+    location.reload();
   }
 }
 
